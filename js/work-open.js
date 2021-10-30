08fcebc38a40d980c89379b8e1c3f64abe67bb2e
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-30 18:57:54 +0900

    .

--- js/work-open.js
+++ js/work-open.js
@@ -1,20 +1,28 @@
 const sectionScroll = document.querySelector('.work-open');
 const btt = document.getElementById('back-to-top');
+let workListEl = document.querySelectorAll('.work-list');
+// exit 버튼을 누르면 work-open창이 닫힌다.
+let exitBtn = document.querySelectorAll('.work-exit span');
 
+
+workListEl = Array.prototype.slice.call(workListEl);
+exitBtn = Array.prototype.slice.call(exitBtn);
+
+
+
+// top버튼 눌렀을 때 상단으로 올라가는 함수 선언
 // 스크롤 양을 저장하는 변수
 let scrollPos;
 // document의 높이를 구해온다.
 let docHeight; 
 
 docHeight = Math.max(sectionScroll.scrollHeight,sectionScroll.offsetHeight);
-
-
 btt.addEventListener('click', (e)=>{
     e.preventDefault();
     scrollToTop();
 });
 function scrollToTop(){
-    var scrollInterval = setInterval(function(){
+    var scrollInterval = setInterval(()=>{
         scrollPos = sectionScroll.scrollTop;
         if(scrollPos != 0){
             sectionScroll.scrollBy(0, -55);
@@ -23,3 +31,24 @@ function scrollToTop(){
         }
      }, 15);
 }
+
+// ------------------------------------------ !
+
+sectionScroll.style.display ='none';
+for(let i= 0; i < workListEl.length; i++){
+    workListEl[i].addEventListener('click',()=>{
+        sectionScroll.style.display = 'block';
+    })
+    workListEl[i].addEventListener('mouseover',()=>{
+        console.log('dd');
+    })
+}
+
+
+for(let i= 0; i < exitBtn.length; i++){
+    exitBtn[i].addEventListener('click',()=>{
+        sectionScroll.style.display = 'none';
+    })
+}
+
+
