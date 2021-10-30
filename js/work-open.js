c3fd5c474dc46dd3d4f058e9b797e7c4e9eac2d7
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-30 18:37:47 +0900

    .

--- js/work-open.js
+++ js/work-open.js
@@ -1,15 +1,12 @@
 const sectionScroll = document.querySelector('.work-open');
 const btt = document.getElementById('back-to-top');
 
-// 일정 위치에 올라오더라도 top 버튼이 나타나게끔
-let offset;
 // 스크롤 양을 저장하는 변수
 let scrollPos;
 // document의 높이를 구해온다.
 let docHeight; 
 
 docHeight = Math.max(sectionScroll.scrollHeight,sectionScroll.offsetHeight);
-
 
 
 btt.addEventListener('click', (e)=>{
