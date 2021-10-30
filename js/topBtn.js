4446a72622554a6a85ef1ebc3e859b067e1e9d7d
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-30 17:58:29 +0900

    .

--- js/topBtn.js
+++ js/topBtn.js
@@ -8,36 +8,19 @@ let scrollPos;
 // document의 높이를 구해온다.
 let docHeight; 
 
-docHeight = Math.max(sectionScroll.scrollHeight,sectionScroll.offsetHeight); 
+docHeight = Math.max(sectionScroll.scrollHeight,sectionScroll.offsetHeight);
 
 
-if(docHeight != 'undifind'){
-    // docHeight 를 4번 나눈 값을 offset에 저장
-    // 4분의 1 값보다 많으면~
-    offset = docHeight / 2;
-}
-
-sectionScroll.addEventListener('scroll', ()=>{
-    // value = sectionScroll.scrollTop;
-    // console.log(value);
-    scrollPos = sectionScroll.scrollTop;
-
-    // btt.className = (scrollPos > offset) ? 'visible' : '';
-
-    if(scrollPos < 400){
-        btt.className ='visible';
-    }
-    console.log(scrollPos);
-})
 
 btt.addEventListener('click', (e)=>{
     e.preventDefault();
-    scrollTop();
+    scrollToTop();
 });
 function scrollToTop(){
     var scrollInterval = setInterval(function(){
+        scrollPos = sectionScroll.scrollTop;
         if(scrollPos != 0){
-            window.scrollBy(0, -55);
+            sectionScroll.scrollBy(0, -55);
         }else{
             clearInterval(scrollInterval);
         }
