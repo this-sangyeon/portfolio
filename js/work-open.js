5e64c12b1d07953409e6598c04b30f88f9917093
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-31 01:37:08 +0900

    .

--- js/work-open.js
+++ js/work-open.js
@@ -3,29 +3,60 @@ const btt = document.getElementById('back-to-top');
 let workListEl = document.querySelectorAll('.work-list li');
 // exit 버튼을 누르면 work-open창이 닫힌다.
 let exitBtn = document.querySelectorAll('.work-exit');
+let workListImage = document.querySelectorAll('.work-listImg div');
+const leftArrowBtn = document.querySelector('.left-arrow');
+const rightArrowBtn = document.querySelector('.right-arrow');
 
-
-const workListImgWrap = document.querySelector('.work-listImg');
-let workListImage = document.querySelectorAll('.bg-workImg');
-
+let workIntroArea = document.querySelectorAll('.work-intro');
+let introLength = workIntroArea.length;
+let currentIndex = 0;
 
 workListEl = Array.prototype.slice.call(workListEl);
 exitBtn = Array.prototype.slice.call(exitBtn);
+workIntroArea = Array.prototype.slice.call(workIntroArea);
+
+
+rightArrowBtn.addEventListener('click',()=>{
+    currentIndex = currentIndex + 1;
+    currentIndex > introLength - 1 && (currentIndex = 0);
+    console.log(currentIndex);
+    for(let i = 0; i < workIntroArea.length; i++){
+        workIntroArea[i].classList.remove('active');
+    }
+    workIntroArea[currentIndex].classList.add('active');
+})
+
+leftArrowBtn.addEventListener('click',()=>{
+    currentIndex = currentIndex - 1;
+    currentIndex < 0 && (currentIndex = introLength - 1);
+    console.log(currentIndex);
+    for(let i = 0; i < workIntroArea.length; i++){
+        workIntroArea[i].classList.remove('active');
+    }
+    workIntroArea[currentIndex].classList.add('active');
+})
+
+
 
 
 // work-area
-
+// workListImage[0].style.zIndex ="2";
 for(let i = 0; i < workListEl.length; i++){
     workListEl[i].addEventListener('mouseenter', (e)=>{
         // console.log('mouse', e.target);
-        workListEl[i].style.color="blue";
+        workListEl[i].style.color="#FF5D2C";
+        for(let j = 0; j < workListImage.length; j++ ){
+            workListImage[j].classList.remove('active');
+        }
+        workListImage[i].classList.add('active');
     })
     workListEl[i].addEventListener('mouseleave', (e)=>{
         // console.log('mouse', e.target);
-        workListEl[i].style.color="white";
+        workListEl[i].style.color ="white";
+        // workListImage[i].classList.remove('active');
     })
-
 }
+
 
 // ------------------------------------------ !
 
@@ -70,6 +101,10 @@ function scrollToTop(){
     }, 15);
 }
 
+
+
+
+ 
 
 
 
